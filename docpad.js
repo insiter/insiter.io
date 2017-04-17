bae092a9bdefa5c49b7dd2770677b510164c7393
Romulo Storel <romulostorel@gmail.com>
2017-04-17 16:17:23 -0300

    Add speakers to docpad.js and enable sections

--- docpad.js
+++ docpad.js
@@ -49,13 +49,13 @@ module.exports = {
     // to deactivate comment out with '//'
     // you can also change order here and it will reflect on page
     sections: [
-      // 'about',
-      // 'location',
-      // 'speakers',
+      'about',
+      'location',
+      'speakers',
       // 'schedule',
       // 'sponsors',
-      // 'partners'
-      // 'contact'
+      // 'partners',
+      'contact'
     ],
 
     // Labels which you can translate to other languages
@@ -76,28 +76,28 @@ module.exports = {
         time: "9h00"
       },
       {
-        name: "Linus Torvalds",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        name: "Daniel Romero",
+        photo: "https://avatars0.githubusercontent.com/u/444911?v=3&s=460",
+        bio: "Desenvolvedor, escritor técnico, palestrante e atualmente trabalha com práticas DevOps.",
         company: "Linux Foundation",
         link: {
-          href: "http://twitter.com/linus",
-          text: "@linus"
+          href: "https://infoslack.pro/",
+          text: "@infoslack"
         },
         presentation: {
-          title: "Digging into a Linux Kernel",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          title: "Unikernels e o futuro da infraestrutura imutável",
+          description: "Eles serão substituídos por hypervisors do tipo bare metal, otimizados de forma específica para o hardware e o deploy de nossas aplicações será feito através de um único arquivo binário. Nesta talk, veremos o que são Unikernels, os motivos de serem rápidos, seguros e que oportunidades ele reserva para o futuro.",
           time: "10h00"
         }
       },
       {
-        name: "Bill Gates",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Microsoft",
+        name: "Fabio Akita",
+        photo: "https://avatars2.githubusercontent.com/u/2840?v=3&s=460",
+        bio: "É co-fundador da boutique de software Codeminer 42. Desde 2008 também é co-organizador da Rubyconf Brasil junto com a Locaweb. Ativista de Ruby on Rails desde 2006, publicou o primeiro livro do assunto. Já realizou quase 200 palestras pelo Brasil e pelo mundo. Trabalha como programador desde o começo dos anos 90 e passou por todas as principais tecnologias do mercado nesse período. Publica artigos no blog akitaonrails.com desde 2006",
+        company: "Codeminer 42",
         link: {
-          href: "http://github.com/billy95",
-          text: "@billy95"
+          href: "http://www.akitaonrails.com/",
+          text: "@akitaOnRails"
         },
         presentation: {
           title: "Introducing Windows 12",
@@ -110,13 +110,28 @@ module.exports = {
         time: "12h00"
       },
       {
-        name: "Chuck Norris",
-        photo: "themes/yellow-swan/img/speaker.jpg",
+        name: "Gabriel Sobrinho",
+        photo: "https://avatars2.githubusercontent.com/u/26460?v=3&s=460",
         bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Delta Command",
+        company: "Hite",
         link: {
-          href: "http://twitter.com/littlechuck",
-          text: "@littlechuck"
+          href: "http://gabrielsobrinho.com/",
+          text: "@sobrinho"
+        },
+        presentation: {
+          title: "Why do I prefer Android over iPhone",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          time: "17h00"
+        }
+      },
+      {
+        name: "Jean Carlo",
+        photo: "https://avatars3.githubusercontent.com/u/353504?v=3&s=460",
+        bio: "Sou graduado em Ciências da Computação pela federal do Rio Grande do Sul e trabalho com desenvolvimento de projetos de internet já há algum tempo. Passei um bom tempo mergulhado em desenvolvimento Ruby on Rails na Codeminer42, em seguida pelo Grupo RBS e por uma Start-up desenvolvendo Go e Android. Atualmente, faço parte da equipe de webmedia da Globo.com.",
+        company: "Globo.com",
+        link: {
+          href: "http://jcemer.com/",
+          text: "@jcemer"
         },
         presentation: {
           title: "How to kill a elephant with one finger",
@@ -125,13 +140,43 @@ module.exports = {
         }
       },
       {
-        name: "Steve Jobs",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Apple, Inc.",
+        name: "Dmitry Rocha",
+        photo: "https://avatars1.githubusercontent.com/u/53300?v=3&s=460",
+        bio: "Desenvolvedor, escritor técnico, palestrante e atualmente trabalha com práticas DevOps.",
+        company: "Codeminer 42",
         link: {
-          href: "http://github.com/stevie",
-          text: "@stevie"
+          href: "https://github.com/dmitryrck",
+          text: "@dmitryrck"
+        },
+        presentation: {
+          title: "Unikernels e o futuro da infraestrutura imutável",
+          description: "Eles serão substituídos por hypervisors do tipo bare metal, otimizados de forma específica para o hardware e o deploy de nossas aplicações será feito através de um único arquivo binário. Nesta talk, veremos o que são Unikernels, os motivos de serem rápidos, seguros e que oportunidades ele reserva para o futuro.",
+          time: "10h00"
+        }
+      },
+      {
+        name: "Matias Leidemer",
+        photo: "https://avatars1.githubusercontent.com/u/10559?v=3&s=460",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        company: "Stickermule",
+        link: {
+          href: "http://www.leidemer.com/",
+          text: "@matiasleidemer"
+        },
+        presentation: {
+          title: "Revealing Facebook Secrets",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          time: "16h00"
+        }
+      },
+      {
+        name: "Thiago Alves",
+        photo: "https://avatars0.githubusercontent.com/u/5190217?v=3&s=460",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        company: "Codeminer 42",
+        link: {
+          href: "https://twitter.com/taltk9",
+          text: "@taltk9"
         },
         presentation: {
           title: "Presenting iPad",
@@ -142,36 +187,6 @@ module.exports = {
       {
         name: "Coffee-break",
         time: "15h00"
-      },
-      {
-        name: "Mark Zuckerberg",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Facebook",
-        link: {
-          href: "http://twitter.com/zuck",
-          text: "@zuck"
-        },
-        presentation: {
-          title: "Revealing Facebook Secrets",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "16h00"
-        }
-      },
-      {
-        name: "Steve Wozniak",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Apple, Inc.",
-        link: {
-          href: "http://twitter.com/woz",
-          text: "@woz"
-        },
-        presentation: {
-          title: "Why do I prefer Android over iPhone",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "17h00"
-        }
       }
     ],
 
