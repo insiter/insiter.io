0dd478ed30ad168b4645f1fa53895e29b1edd1f9
Abner Soares Alves Junior <abnersajr@gmail.com>
2017-11-23 17:42:00 -0200

    Add map start

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,6 +2,20 @@
 
 import components from './components'
 
+window.initMap = function() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -30.035292, lng: -51.226415 },
+    zoom: 15,
+    scrollwheel: false
+  });
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(-25.363882, 131.044922),
+    map: map
+  });
+  marker.setMap(map);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   components.forEach(component => component())
 })
