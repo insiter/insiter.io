76da3c5c0065d182395d99a2850eeb902c7697f6
Will Soares <willame_soares@hotmail.com>
2018-04-07 01:18:57 -0300

    Archive layout for Goiania edition

---
+++ source/2018/goiania/javascripts/map.js
@@ -0,0 +1,22 @@
+export default () => {
+  const mapContainer = document.querySelector('[data-google-maps]')
+  const { lat, lng } = mapContainer.dataset
+  const center = {
+    lat: Number(lat),
+    lng: Number(lng)
+  }
+
+  /* global google */
+  const map = new google.maps.Map(mapContainer, {
+    zoom: 15,
+    scrollwheel: false,
+    styles: require('./map-style.json'),
+    center
+  })
+
+  /* eslint-disable no-new */
+  new google.maps.Marker({
+    position: center,
+    map
+  })
+}
---
+++ source/2018/goiania/javascripts/site.js
@@ -0,0 +1,1 @@
+window.mapsApiReady = require('./map').default
